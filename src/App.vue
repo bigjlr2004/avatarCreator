72d10a039947c82030e575ef3757a0f8110a0340
John Roberts <bigjlr2004@yahoo.com>
2023-06-19 20:06:54 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import AvatarForm from './components/AvatarForm.vue'
+import AvatarImage from './components/AvatarImage.vue'
+</script>
+
+<template>
+  <main>
+    <div>Avatar Maker App</div>
+    <AvatarForm />
+    <AvatarImage />
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 65rem;
+  margin: 2rem auto;
+}
+</style>
